41581f50288513b2f2b3645d20d2f2b43dd13a7a
Sashankdeva <sashankdevarakonda@gmail.com>
2026-07-17 15:52:15 +0530

    fix: resolve missing import causing Flutter build failure

--- attendance_admin_app/lib/screens/recovery_wrapper.dart
+++ attendance_admin_app/lib/screens/recovery_wrapper.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/attendance_provider.dart';
 import '../services/session_recovery_service.dart';
+import 'faculty_workspace_screen.dart';
 import 'scanner_screen.dart';
 
 class RecoveryWrapper extends StatefulWidget {
